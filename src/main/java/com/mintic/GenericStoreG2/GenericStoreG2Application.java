4c5e6e33c7a2adb8563b7f88e684a77b63a39900
Julio Bermudez <julenbero@gmail.com>
2021-09-23 13:50:26 -0500

    Creacion del Proyecto

---
+++ src/main/java/com/mintic/GenericStoreG2/GenericStoreG2Application.java
@@ -0,0 +1,13 @@
+package com.mintic.GenericStoreG2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericStoreG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericStoreG2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mintic/GenericStoreG2/GenericStoreG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintic.GenericStoreG2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericStoreG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
