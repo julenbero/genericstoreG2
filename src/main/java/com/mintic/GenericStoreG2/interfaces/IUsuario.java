1ee59bd4a7dcbbf591f0ae054762a12925d13f46
Julio Bermudez <julenbero@gmail.com>
2021-09-27 22:20:49 -0500

    Creacion Modelo Usuario, Creacion IUsuario, Controlador

---
+++ src/main/java/com/mintic/GenericStoreG2/interfaces/IUsuario.java
@@ -0,0 +1,9 @@
+package com.mintic.GenericStoreG2.interfaces;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mintic.GenericStoreG2.model.Usuario;
+
+public interface IUsuario extends CrudRepository<Usuario, Long>{
+
+}
